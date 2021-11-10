994bbbb2b6794240af17e33a2bcb898b8afc863e
Luis Garcia Regardiz <gadeeks@gmail.com>
2021-11-09 22:27:45 -0400

    Fix bugs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 app = FastAPI()
 
 client_url = os.getenv("CLIENT_URL")
-origins = [client_url, "http://localhost", "http://localhost:8000"]
+origins = [client_url, "http://localhost", "http://localhost:8000", "https://fastapi-taski.herokuapp.com"]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
